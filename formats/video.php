1fb39a1a098ce78a67c5b23320d5bfed93dce24b
Sergey Kambalin <greyexpert@gmail.com>
2015-05-05 12:13:29 +0600

    Video format refactoring

--- formats/video.php
+++ formats/video.php
@@ -2,13 +2,18 @@
 
 class NEWSFEED_FORMAT_Video extends NEWSFEED_CLASS_Format
 {
-    public function onBeforeRender()
+    protected $uniqId;
+    
+    public function __construct($vars, $formatName = null) 
     {
-        parent::onBeforeRender();
+        parent::__construct($vars, $formatName);
         
-        $uniqId = uniqid("vf-");
-        $this->assign("uniqId", $uniqId);
-
+        $this->uniqId = uniqid("vf-");
+        $this->vars = $this->prepareVars($vars);
+    }
+    
+    protected function prepareVars( $vars )
+    {
         $defaults = array(
             "image" => null,
             "iconClass" => null,
@@ -19,29 +24,41 @@ class NEWSFEED_FORMAT_Video extends NEWSFEED_CLASS_Format
             "embed" => ''
         );
 
-        $tplVars = array_merge($defaults, $this->vars);
+        $out = array_merge($defaults, $vars);
+        $out["url"] = $this->getUrl($out["url"]);
+        $out['blankImg'] = OW::getThemeManager()->getCurrentTheme()->getStaticUrl() . 'mobile/images/1px.png';
         
-        $tplVars["url"] = $this->getUrl($tplVars["url"]);
-        $tplVars['blankImg'] = OW::getThemeManager()->getCurrentTheme()->getStaticUrl() . 'mobile/images/1px.png';
+        return $out;
+    }
+
+    protected function initJs()
+    {
+        $js = UTIL_JsGenerator::newInstance();
         
-        $this->assign('vars', $tplVars);
+        $code = BOL_TextFormatService::getInstance()->addVideoCodeParam($this->vars['embed'], "autoplay", 1);
+        $code = BOL_TextFormatService::getInstance()->addVideoCodeParam($code, "play", 1);
+
+        $js->addScript('$(".ow_oembed_video_cover", "#" + {$uniqId}).click(function() { '
+                . '$("#" + {$uniqId}).addClass("ow_video_playing"); '
+                . '$(".ow_newsfeed_item_picture", "#" + {$uniqId}).html({$embed});'
+                . 'return false; });', array(
+            "uniqId" => $this->uniqId,
+            "embed" => $code
+        ));
+
+        OW::getDocument()->addOnloadScript($js);
+    }
+    
+    public function onBeforeRender()
+    {
+        parent::onBeforeRender();
         
-        if ( $tplVars['embed'] )
+        $this->assign("uniqId", $this->uniqId);
+        $this->assign('vars', $this->vars);
+        
+        if ( $this->vars['embed'] )
         {
-            $js = UTIL_JsGenerator::newInstance();
-        
-            $code = BOL_TextFormatService::getInstance()->addVideoCodeParam($tplVars['embed'], "autoplay", 1);
-            $code = BOL_TextFormatService::getInstance()->addVideoCodeParam($code, "play", 1);
-
-            $js->addScript('$(".ow_oembed_video_cover", "#" + {$uniqId}).click(function() { '
-                    . '$("#" + {$uniqId}).addClass("ow_video_playing"); '
-                    . '$(".ow_newsfeed_item_picture", "#" + {$uniqId}).html({$embed});'
-                    . 'return false; });', array(
-                "uniqId" => $uniqId,
-                "embed" => $code
-            ));
-
-            OW::getDocument()->addOnloadScript($js);
+            $this->initJs();
         }
     }
 }
